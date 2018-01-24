6d4202256615bbabe631510eefb8d5f45578ba77
martin <mvarmuza@gmail.com>
2018-01-24 12:56:21 +0100

    added basic write protection security rules

---
+++ packages/app/src/errors/CustomError.js
@@ -0,0 +1,8 @@
+/*
+just a custom error class that will allow me to extend error handling without
+heavy refactoring in future
+ */
+
+class CustomError extends Error {}
+
+export default CustomError
