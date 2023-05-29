7ab8b93232db71e17bca9f6f57b21769e9128962
Martin Varmuza <mvarmuza@gmail.com>
2023-05-29 17:50:11 +0200

    yarn 3 and lifemonitor fix

--- web/src/modules/LifeMonitor.tsx
+++ web/src/modules/LifeMonitor.tsx
@@ -1,17 +1,34 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
-import { useMemo } from 'react';
+import { useEffect, useMemo, useState} from 'react';
 
 import { Control } from '../components';
 
 export const LifeMonitor = ({ data, user, dbRef }) => {
+    const [now, setNow] = useState(Date.now());
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setNow(Date.now());
+        }, 1000)
+
+        return () => {
+            clearInterval(interval);
+        }
+    }, [])
+
     const state = useMemo(() => {
         if (!data) {
-            return
+            return 'loading...'
         }
         const { lifeMonitor } = data;
-        return lifeMonitor;
-    }, [data])
 
+        if (lifeMonitor) {
+            const diff = now - lifeMonitor;
+            return `${Math.min(Math.floor(diff / 1000), 1)} seconds ago`;
+        } else {
+            return 'no data';
+        }
+    }, [now, data]);
 
     return (
         <Control name="Last ping" state={state} actions={[]} />
